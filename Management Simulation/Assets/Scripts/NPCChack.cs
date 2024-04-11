f5444d0de724efac76e5b3370bcb65690aec4b75
Ritaera <154584398+Ritaera@users.noreply.github.com>
2024-04-11 15:19:09 +0900

    게임 개발 8일차
    파일 합치기용 업로드.

--- Management Simulation/Assets/Scripts/NPCChack.cs
+++ Management Simulation/Assets/Scripts/NPCChack.cs
@@ -5,59 +5,9 @@ using UnityEngine;
 public class NPCChack : MonoBehaviour
 {
     PlayerController2 _playerController;
-    UIUpgrade _uIUpgrade;
-    public UpgradeScriptableObject upgradeScriptableObject;
 
-    private void Awake()
-    {
-        if (_playerController == null)
-        {
-            _playerController = FindFirstObjectByType<PlayerController2>();
-        }
-        if (_uIUpgrade == null)
-        {
-            _uIUpgrade = FindFirstObjectByType<UIUpgrade>();
-        }
-        if (upgradeScriptableObject == null)
-        {
-            upgradeScriptableObject = FindFirstObjectByType<UpgradeScriptableObject>();
-        }
-    }
     public void Upgrade()
     {
-        if (_playerController.HitName == "Sam smith") //Todo: 대장간 작업 처리.
-        {
-            _uIUpgrade.PopUpBase.SetActive(true);
-            _upgradeDescription.text = upgradeScriptableObject.upGradeDescription;  // 업그레이드 정보.
-
-            _upgradePoint.text = $"<color=yellow>매턴 골드 : {upgradeScriptableObject.upGradeEveryTurnGold}</color> / <color=pink>행복도 : {upgradeScriptableObject.upGradeHappyAffectValue}</color> / <color=blue>치안 : {upgradeScriptableObject.upGradeSafetyAffectValue}</color> / <color=white>신앙 : {upgradeScriptableObject.upGradeFaithAffectValue}</color> / <color=purple>문화 : {upgradeScriptableObject.upGradeCulturalAffectValue}</color>";
-
-            _upgradeCost.text = $"비용 : {upgradeScriptableObject.upGradeGold}";
-
-        }
-        else if (_playerController.HitName == "Maria") //Todo: 성당처리
-        {
-            _uIUpgrade.PopUpBase.SetActive(true);
-
-            
-        }
-        else if(_playerController.HitName == "Hani") //Todo: 모럼가길드
-        {
-            _uIUpgrade.PopUpBase.SetActive(true);
-
-            
-        }
-        else if (_playerController.HitName == "Hyeji") //Todo: 성
-        {
-            _uIUpgrade.PopUpBase.SetActive(true);
-
-            
-        }
-        else
-        {
-            _uIUpgrade.PopUpBase.SetActive(true); //Todo: 은행
-
-
-        }
+        
     }
 }
--- Management Simulation/Assets/Scripts/UI/UIUpgrade.cs
+++ Management Simulation/Assets/Scripts/UI/UIUpgrade.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,6 +15,12 @@ public class UIUpgrade : MonoBehaviour
     private Button _accapt; // 강화수락 버튼
     private Button _cancel; // 강화취소 버튼
 
+    private int _samUpgradeCount = 0;
+    private int _mariaUpgradeCount = 0;
+    private int _heyjiUpgradeCount = 0;
+    private int _haniUpgradeCount = 0;
+    private int _banksyUpgradeCount = 0;
+
     public GameObject PopUpBase
     {
         get
@@ -26,13 +33,18 @@ public class UIUpgrade : MonoBehaviour
         }
     }
 
-    public UpgradeScriptableObject upgradeScriptableObject;
+    UpgradeScriptableObject upgradeScriptableObject;
+    PlayerController2 _playerController;
 
     private void Awake()
     {
         if (upgradeScriptableObject == null)
         {
             upgradeScriptableObject = FindFirstObjectByType<UpgradeScriptableObject>();
+        }
+        if (_playerController == null)
+        {
+            _playerController = FindFirstObjectByType<PlayerController2>();
         }
 
         _popUpBase = transform.Find("Panel - PopUpBase").GetComponent<GameObject>();
@@ -43,6 +55,181 @@ public class UIUpgrade : MonoBehaviour
         _cancel = transform.Find("Panel - PopUpBase/Image - Chat/Button - Cancel").GetComponent<Button>();
     }
 
+    public void Upgrade()
+    {
+        if (_playerController.HitName == "Sam smith") //Todo: 대장간 작업 처리.
+        {
+            PopUpBase.SetActive(true);
+            
+            switch (_samUpgradeCount)
+            {
+                case 0:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/blacksmithLevel1");
+                    UpdateText();
+                    break;
+                case 1:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/blacksmithLevel2");
+                    UpdateText();
+                    break;
+                case 2:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/blacksmithLevel3");
+                    UpdateText();
+                    break;
+                case 3:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/blacksmithLevel4");
+                    UpdateText();
+                    break;
+                case 4:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/blacksmithLevel5");
+                    UpdateText();
+                    break;
+                case 5:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/blacksmithLevel6");
+                    UpdateText();
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if (_playerController.HitName == "Maria") //Todo: 성당처리
+        {
+            PopUpBase.SetActive(true);
+            UpdateText();
+            switch (_mariaUpgradeCount)
+            {
+                case 0:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/Cathedral1");
+                    UpdateText();
+                    break;
+                case 1:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/Cathedral2");
+                    UpdateText();
+                    break;
+                case 2:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/Cathedral3");
+                    UpdateText();
+                    break;
+                case 3:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/Cathedral4");
+                    UpdateText();
+                    break;
+                case 4:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/Cathedral5");
+                    UpdateText();
+                    break;
+                case 5:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/Cathedral6");
+                    UpdateText();
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if (_playerController.HitName == "Hani") //Todo: 모럼가길드
+        {
+            PopUpBase.SetActive(true);
+            UpdateText();
+            switch (_haniUpgradeCount)
+            {
+                case 0:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/GuildLevel1");
+                    UpdateText();
+                    break;
+                case 1:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/GuildLevel2");
+                    UpdateText();
+                    break;
+                case 2:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/GuildLevel3");
+                    UpdateText();
+                    break;
+                case 3:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/GuildLevel4");
+                    UpdateText();
+                    break;
+                case 4:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/GuildLevel5");
+                    UpdateText();
+                    break;
+                case 5:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/GuildLevel6");
+                    UpdateText();
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if (_playerController.HitName == "Hyeji") //Todo: 성
+        {
+            PopUpBase.SetActive(true);
+            UpdateText();
+            switch (_heyjiUpgradeCount)
+            {
+                case 0:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/CastleLevel1");
+                    UpdateText();
+                    break;
+                case 1:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/CastleLevel2");
+                    UpdateText();
+                    break;
+                case 2:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/CastleLevel3");
+                    UpdateText();
+                    break;
+                case 3:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/CastleLevel4");
+                    UpdateText();
+                    break;
+                case 4:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/CastleLevel5");
+                    UpdateText();
+                    break;
+                case 5:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/CastleLevel6");
+                    UpdateText();
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            PopUpBase.SetActive(true); //Todo: 은행
+            UpdateText();
+            switch (_banksyUpgradeCount)
+            {
+                case 0:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/Bank1");
+                    UpdateText();
+                    break;
+                case 1:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/Bank2");
+                    UpdateText();
+                    break;
+                case 2:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/Bank3");
+                    UpdateText();
+                    break;
+                case 3:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/Bank4");
+                    UpdateText();
+                    break;
+                case 4:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/Bank5");
+                    UpdateText();
+                    break;
+                case 5:
+                    Resources.Load<UpgradeScriptableObject>("UpgradeScriptableObject/Bank6");
+                    UpdateText();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+
     private void Start()
     {
         _accapt.onClick.AddListener(AccaptButtonClick);
@@ -51,14 +238,33 @@ public class UIUpgrade : MonoBehaviour
 
     public void AccaptButtonClick()
     {
-        _upgradeDescription.text = upgradeScriptableObject.upGradeDescription;  // 업그레이드 정보.
-
-        _upgradePoint.text = $"<color=yellow>매턴 골드 : {upgradeScriptableObject.upGradeEveryTurnGold}</color> / <color=pink>행복도 : {upgradeScriptableObject.upGradeHappyAffectValue}</color> / <color=blue>치안 : {upgradeScriptableObject.upGradeSafetyAffectValue}</color> / <color=white>신앙 : {upgradeScriptableObject.upGradeFaithAffectValue}</color> / <color=purple>문화 : {upgradeScriptableObject.upGradeCulturalAffectValue}</color>";
+        if (GameManager.instance.Gold >= upgradeScriptableObject.upGradeGold)
+        {
+            
+        }
+        else
+        {
+            _upgradeCost.text = "<color=red>골드가 부족합니다.</color>";
+            _accapt.interactable = false;
+        }
         
-        _upgradeCost.text = $"비용 : {upgradeScriptableObject.upGradeGold}";
     }
     public void CancelButtonClick()
     {
         _popUpBase.SetActive(false);
+    }
+
+
+
+
+
+
+    public void UpdateText()
+    {
+        _upgradeDescription.text = upgradeScriptableObject.upGradeDescription;  // 업그레이드 정보.
+
+        _upgradePoint.text = $"<color=yellow>매턴 골드 : {upgradeScriptableObject.upGradeEveryTurnGold}</color> / <color=pink>행복도 : {upgradeScriptableObject.upGradeHappyAffectValue}</color> / <color=blue>치안 : {upgradeScriptableObject.upGradeSafetyAffectValue}</color> / <color=white>신앙 : {upgradeScriptableObject.upGradeFaithAffectValue}</color> / <color=purple>문화 : {upgradeScriptableObject.upGradeCulturalAffectValue}</color>";
+
+        _upgradeCost.text = $"비용 : {upgradeScriptableObject.upGradeGold}";
     }
 }
