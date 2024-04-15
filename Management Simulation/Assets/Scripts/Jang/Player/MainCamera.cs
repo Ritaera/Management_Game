a310a8c3dea5d5c4a4b50b457165208a6147d4a7
Udit3991 <154584627+Udit3991@users.noreply.github.com>
2024-04-15 16:36:09 +0900

    BG
    dsasd

--- Management Simulation/Assets/Scripts/Jang/Player/MainCamera.cs
+++ Management Simulation/Assets/Scripts/Jang/Player/MainCamera.cs
@@ -1,28 +1,30 @@
 using UnityEngine;
 
-public class MainCamera : MonoBehaviour
+namespace Assets.Scripts.Jang.Player
 {
-    public Transform _playerTransform;
-    [SerializeField] private float _smoothing = 0.2f;
-    private Transform _minCameraBoundary;
-    private Transform _maxCameraBoundary;
-
-    private void Awake()
+    public class MainCamera : MonoBehaviour
     {
-        _playerTransform = GameObject.Find("Player").GetComponent<Transform>();
-        _minCameraBoundary = GameObject.Find("MinValueCamera").GetComponent<Transform>();
-        _maxCameraBoundary = GameObject.Find("MaxValueCamera").GetComponent<Transform>();
+        public Transform _playerTransform;
+        [SerializeField] private float _smoothing = 0.2f;
+        private Transform _minCameraBoundary;
+        private Transform _maxCameraBoundary;
+
+        private void Awake()
+        {
+            _playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+            _minCameraBoundary = GameObject.Find("MinValueCamera").GetComponent<Transform>();
+            _maxCameraBoundary = GameObject.Find("MaxValueCamera").GetComponent<Transform>();
+        }
+
+        private void LateUpdate()
+        {
+            Vector3 targetPos = new Vector3(_playerTransform.position.x, _playerTransform.position.y, transform.position.z);
+
+            targetPos.x = Mathf.Clamp(targetPos.x, _minCameraBoundary.position.x, _maxCameraBoundary.position.x);
+            targetPos.y = Mathf.Clamp(targetPos.y, _minCameraBoundary.position.y, _maxCameraBoundary.position.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPos, _smoothing);
+        }
+
     }
-
-    private void LateUpdate()
-    {
-        Vector3 targetPos = new Vector3(_playerTransform.position.x, _playerTransform.position.y, this.transform.position.z);
-
-        targetPos.x = Mathf.Clamp(targetPos.x, _minCameraBoundary.position.x, _maxCameraBoundary.position.x);
-        targetPos.y = Mathf.Clamp(targetPos.y, _minCameraBoundary.position.y, _maxCameraBoundary.position.y);
-
-        transform.position = Vector3.Lerp(transform.position, targetPos, _smoothing);
-    }
-    
 }
-    
