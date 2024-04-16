a826913d2db1f4918d73c2496015b2f2875e8526
Udit3991 <154584627+Udit3991@users.noreply.github.com>
2024-04-16 14:22:14 +0900

    BG
    asd

--- Management Simulation/Assets/Scripts/BGScroll.cs
+++ Management Simulation/Assets/Scripts/BGScroll.cs
@@ -1,35 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BGScroll : MonoBehaviour
 {
     public float speed;
-    public Transform[] backgrounds;
+    public Transform[] backgrounds; // 배경 배열
 
-    float leftPosX = 0f;
-    float rightPosX = 0f;
-    float xScreenHalfSize;
-    float yScreenHalfSize;
+    private float imageWidth; // 배경 이미지의 너비
+    private float cameraWidth;
+
     void Start()
     {
-        yScreenHalfSize = Camera.main.orthographicSize;
-        xScreenHalfSize = yScreenHalfSize * Camera.main.aspect;
+        // 첫 번째 배경의 SpriteRenderer 컴포넌트를 사용하여 너비 계산
+        imageWidth = backgrounds[0].GetComponent<SpriteRenderer>().bounds.size.x;
 
-        leftPosX = -(xScreenHalfSize * 2);
-        rightPosX = xScreenHalfSize * 2 * backgrounds.Length;
-    }
-    void Update()
-    {
+        // 카메라의 너비 계산
+        cameraWidth = Camera.main.orthographicSize * Camera.main.aspect;
+
+        // 배경 이미지들을 연속적으로 배치
+        float totalWidth = imageWidth * backgrounds.Length;
+        float startX = -(totalWidth / 2) + (imageWidth / 2);
         for (int i = 0; i < backgrounds.Length; i++)
         {
-            backgrounds[i].position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+            backgrounds[i].position = new Vector3(startX + i * imageWidth, backgrounds[i].position.y, backgrounds[i].position.z);
+        }
+    }
 
-            if (backgrounds[i].position.x < leftPosX)
+    void Update()
+    {
+        // 모든 배경을 왼쪽으로 이동
+        foreach (Transform background in backgrounds)
+        {
+            background.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+
+            // 이미지가 카메라 뷰 왼쪽 경계를 완전히 벗어날 경우
+            if (background.position.x < Camera.main.transform.position.x - cameraWidth - (imageWidth / 2))
             {
-                Vector3 nextPos = backgrounds[i].position;
-                nextPos = new Vector3(nextPos.x + rightPosX, nextPos.y, nextPos.z);
-                backgrounds[i].position = nextPos;
+                // 가장 오른쪽에 위치한 배경 이미지를 왼쪽으로 이동
+                float rightMostX = 0;
+                foreach (Transform bg in backgrounds)
+                {
+                    if (bg.position.x > rightMostX)
+                    {
+                        rightMostX = bg.position.x;
+                    }
+                }
+                background.position = new Vector3(rightMostX + imageWidth, background.position.y, background.position.z);
             }
         }
     }
