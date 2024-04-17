1503fd6cba3de66539639a3cde7d093e2ecc80ac
Ritaera <154584398+Ritaera@users.noreply.github.com>
2024-04-17 14:38:10 +0900

    asd
    asd

--- Management Simulation/Assets/01.Scripts/BGScroll.cs
+++ Management Simulation/Assets/01.Scripts/BGScroll.cs
@@ -1,35 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BGScroll : MonoBehaviour
 {
     public float speed;
-    public Transform[] backgrounds;
+    public Transform[] backgrounds; // 배경 배열
 
-    float leftPosX = 0f;
-    float rightPosX = 0f;
-    float xScreenHalfSize;
-    float yScreenHalfSize;
+    private float imageWidth; // 배경 이미지의 너비
+    private float cameraWidth;
+
     void Start()
     {
-        yScreenHalfSize = Camera.main.orthographicSize;
-        xScreenHalfSize = yScreenHalfSize * Camera.main.aspect;
+        // 첫 번째 배경의 SpriteRenderer 컴포넌트를 사용하여 너비 계산
+        imageWidth = backgrounds[0].GetComponent<SpriteRenderer>().bounds.size.x;
 
-        leftPosX = -(xScreenHalfSize * 2);
-        rightPosX = xScreenHalfSize * 2 * backgrounds.Length;
-    }
-    void Update()
-    {
+        // 카메라의 너비 계산
+        cameraWidth = Camera.main.orthographicSize * Camera.main.aspect;
+
+        // 배경 이미지들을 카메라에 붙이기
         for (int i = 0; i < backgrounds.Length; i++)
         {
-            backgrounds[i].position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+            backgrounds[i].parent = Camera.main.transform;
+        }
 
-            if (backgrounds[i].position.x < leftPosX)
+        // 배경 이미지들을 연속적으로 배치
+        float totalWidth = imageWidth * (backgrounds.Length - 1); // 마지막 이미지는 첫 번째 이미지와 겹치므로 하나를 제외
+        float startX = -(totalWidth / 2);
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            backgrounds[i].localPosition = new Vector3(startX + i * imageWidth, backgrounds[i].localPosition.y, backgrounds[i].localPosition.z);
+        }
+    }
+
+    void Update()
+    {
+        // 배경 이미지들을 왼쪽으로 이동
+        foreach (Transform background in backgrounds)
+        {
+            background.localPosition += new Vector3(-speed, 0, 0) * Time.deltaTime;
+
+            // 이미지가 왼쪽 경계를 벗어났는지 확인
+            if (background.localPosition.x + (imageWidth / 2) < -cameraWidth)
             {
-                Vector3 nextPos = backgrounds[i].position;
-                nextPos = new Vector3(nextPos.x + rightPosX, nextPos.y, nextPos.z);
-                backgrounds[i].position = nextPos;
+                // 오른쪽 끝으로 이동
+                float rightMostX = 0;
+                foreach (Transform bg in backgrounds)
+                {
+                    if (bg.localPosition.x > rightMostX)
+                    {
+                        rightMostX = bg.localPosition.x;
+                    }
+                }
+                background.localPosition = new Vector3(rightMostX + imageWidth, background.localPosition.y, background.localPosition.z);
             }
         }
     }
